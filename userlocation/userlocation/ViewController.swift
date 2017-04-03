7d3573aa5ec1366f670f297f2d9a78adc35f8463
chenry053 <chenry053@gmail.com>
2017-04-03 17:42:23 -0600

    initial setup
    got the map to display current location, and I have the coordinates printing out to console of current location and the opposite location as well. Trying to figure out what to do with them next

---
+++ userlocation/userlocation/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  userlocation
+//
+//  Created by Student on 3/31/17.
+//  Copyright © 2017 Student. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class ViewController: UIViewController, CLLocationManagerDelegate{
+    
+    //Map
+    @IBOutlet weak var map: MKMapView!
+    
+    let manager = CLLocationManager()
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[0]
+        
+        let span:MKCoordinateSpan = MKCoordinateSpanMake(0.01,0.01)
+        let myLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        let region:MKCoordinateRegion = MKCoordinateRegionMake(myLocation, span)
+        map.setRegion(region, animated:true)
+        
+        print(location.coordinate)
+        print((-location.coordinate.latitude),(location.coordinate.longitude+180))
+        
+        self.map.showsUserLocation = true
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+
+    }
+
+
+}
+
